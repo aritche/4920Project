4d733c90fb737af6f06f4ce43685f643b214e55a
Ari <ari.tchetchenian@gmail.com>
2018-09-09 03:47:28 +1000

    Small format changes to login/signup forms. Error messages too.

--- front-end/src/pages/login/CreateLoginForm.js
+++ front-end/src/pages/login/CreateLoginForm.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import { Button, Form, Grid } from 'semantic-ui-react';
+import { Button, Form, Grid, Message } from 'semantic-ui-react';
 
 export default class CreateLoginForm extends Component {
     constructor() {
         super();
         
         this.state = {
-            username: '',
+            email: '',
             password: ''
         }
     }
     
-    onUsernameChange = (e) => {
-        this.setState({ username: e.target.value });
+    onEmailChange = (e) => {
+        this.setState({ email: e.target.value });
     }
 
     onPasswordChange = (e) => {
@@ -20,7 +20,7 @@ export default class CreateLoginForm extends Component {
     }
     
     attemptLogin = () => {
-        alert(this.state.username + " has logged in with password " + this.state.password)
+        alert(this.state.email + " has logged in with password " + this.state.password)
     }
     
     render() {
@@ -30,12 +30,13 @@ export default class CreateLoginForm extends Component {
                 <h1>Log in</h1>
                 <Form>
                     <Form.Field>
-                        <Form.Input placeholder="Email" value={this.state.username} onChange={this.onUsernameChange} />
+                        <Form.Input placeholder="Email" value={this.state.email} onChange={this.onEmailChange} />
                         <Form.Input placeholder="Password" type='password' value={this.state.password} onChange={this.onPasswordChange} />
                     </Form.Field>
                     <Button color='green' type='submit' onClick={this.attemptLogin}>Log In</Button>
                 </Form>
                 </Grid.Row>
+                <Message error header='Unable to Log In' content='This is where an error will be displayed if login is unsuccessful'/>
             </Grid>
         )
     }
--- front-end/src/pages/signup/CreateSignupForm.js
+++ front-end/src/pages/signup/CreateSignupForm.js
@@ -1,41 +1,53 @@
 import React, { Component } from 'react';
-import { Button, Form, Grid } from 'semantic-ui-react';
+import { Button, Form, Grid, Message } from 'semantic-ui-react';
 
 export default class CreateSignupForm extends Component {
     constructor() {
         super();
         
         this.state = {
-            username: '',
+            firstName: '',
+            lastName: '',
+            email: '',
             password: ''
         }
     }
+    onFirstNameChange = (e) => {
+        this.setState({ firstName: e.target.value });
+    }
+
+    onLastNameChange = (e) => {
+        this.setState({ lastName: e.target.value });
+    }
     
-    onUsernameChange = (e) => {
-        this.setState({ username: e.target.value });
+    onEmailChange = (e) => {
+        this.setState({ email: e.target.value });
     }
 
     onPasswordChange = (e) => {
         this.setState({ password: e.target.value });
     }
     
-    attemptLogin = () => {
-        alert(this.state.username + " has logged in with password " + this.state.password)
+    attemptSignup = () => {
+        alert(this.state.firstName + " " + this.state.lastName + " has signed up with " + this.state.email + " and password: " + this.state.password)
     }
     
     render() {
         return (
             <Grid centered columns={2}>
                 <Grid.Row centered columns={2}>
-                <h1>Log in</h1>
+                <h1>Join the Community!</h1>
                 <Form>
-                    <Form.Field>
-                        <Form.Input placeholder="Email" value={this.state.username} onChange={this.onUsernameChange} />
-                        <Form.Input placeholder="Password" type='password' value={this.state.password} onChange={this.onPasswordChange} />
-                    </Form.Field>
-                    <Button color='green' type='submit' onClick={this.attemptLogin}>Log In</Button>
+                    <Form.Group widths='equal'>
+                        <Form.Input placeholder="First Name" value={this.state.firstName} onChange={this.onFirstNameChange} />
+                        <Form.Input placeholder="Last Name" value={this.state.lastName} onChange={this.onLastNameChange} />
+                    </Form.Group>
+                    <Form.Input placeholder="Email" value={this.state.email} onChange={this.onEmailChange} />
+                    <Form.Input placeholder="Password" type='password' value={this.state.password} onChange={this.onPasswordChange} />
+                    <Button color='green' type='submit' onClick={this.attemptSignup}>Sign up</Button>
                 </Form>
                 </Grid.Row>
+                <Message error header='Unable to Sign Up' content='This is where an error will be displayed if signup is unsuccessful'/>
             </Grid>
         )
     }
