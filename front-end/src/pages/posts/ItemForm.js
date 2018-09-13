396eb34c108ff1670111516ebb64bf3c87557b39
Jimmy Huang <jimmy@Jimmys-MacBook-Pro.local>
2018-09-13 11:36:56 +1000

    Implement item add

--- front-end/src/pages/posts/ItemForm.js
+++ front-end/src/pages/posts/ItemForm.js
@@ -6,14 +6,8 @@ import { Form, TextArea, Header, Modal, Button, Icon } from 'semantic-ui-react';
  */
 export default class ItemForm extends Component {
 
-    onChange = (e) => {
-        this.setState({[e.target.name]: e.target.value});
-    };
-
-    onSumbit = (e) => {
-        alert(e.target.value.volume);
-        this.props.handleAdd(e.target.value);
-        e.preventDefault();
+    onSubmit = () => {
+      this.props.addItem();
     };
 
     render() {
@@ -30,20 +24,20 @@ export default class ItemForm extends Component {
             <Modal.Content>
               <Form>
                 <Form.Group widths='equal'>
-                  <Form.Input fluid label='Name'  placeholder='Item Name' onNameChange={this.onChange}/>
-                  <Form.Input fluid label='Estimated Weight'  placeholder='Item Weight' onNameChange={this.onChange}/>
-                  <Form.Input fluid label='Estimated Volume'  placeholder='Item Volume' onNameChange={this.onChange}/>
-                  <Form.Input fluid label='Amount'  placeholder='Item Amount' onNameChange={this.onChange}/>
+                  <Form.Input fluid label='Name'  placeholder='Item Name' onChange={(e) => this.props.onChange('name', e.target.value)}/>
+                  <Form.Input fluid label='Estimated Weight'  placeholder='Item Weight' onChange={(e) => this.props.onChange('weight', e.target.value)}/>
+                  <Form.Input fluid label='Estimated Volume'  placeholder='Item Volume' onChange={(e) => this.props.onChange('volume', e.target.value)}/>
+                  <Form.Input fluid label='Amount'  placeholder='Item Amount' onChange={(e) => this.props.onChange('amount', e.target.value)}/>
                 </Form.Group>
                 <Header content={'Item Description'} />
-                <TextArea autoHeight onNameChange={this.onChange}/>
+                <TextArea autoHeight onChange={(e) => this.props.onChange('desc', e.target.value)}/>
               </Form>
             </Modal.Content>
             <Modal.Actions>
-              <Button color='green' onClick={this.onSumbit}>
+              <Button color='green' onClick={this.onSubmit}>
                 <Icon name='checkmark' /> Yes
               </Button>
-              <Button color='red'>
+              <Button color='red' >
                 <Icon name='remove' /> No
               </Button>
             </Modal.Actions>
