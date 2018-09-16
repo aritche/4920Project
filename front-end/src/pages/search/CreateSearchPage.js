0a4c311232283dc116c560fefba35022c03a16f9
Ari <ari.tchetchenian@gmail.com>
2018-09-17 04:53:57 +1000

    Added sort by UI to search results page

--- front-end/src/pages/search/CreateSearchPage.js
+++ front-end/src/pages/search/CreateSearchPage.js
@@ -1,7 +1,43 @@
 import React, { Component } from 'react';
-import { Button, Form, Grid, Message, Segment, Dropdown } from 'semantic-ui-react';
+import { Button, Form, Grid, Message, Segment, Dropdown, Menu, Label } from 'semantic-ui-react';
 import InputSlider from '../../widgets/InputSlider'
 import { BUDGET } from '../../constants';
+import PostList from '../posts/PostList';
+
+const results = [
+    {
+        id: 1,
+        user: "John Smith",
+        title: "Looking for removalist",
+        date: "10/09/18",
+        budget: "$1000",
+        addressTo: "Suburb 1",
+        addressFrom: "Suburb 2",
+        description: `Hi, I am looking for a removalist
+        the printing and typesetting 
+        industry. Lorem Ipsum has been
+        the industry's standard dummy text 
+        ever since the 1500s, when an unknown
+        printer took a galley of type and scrambled 
+        it to make a type specimen book`
+    },
+    {
+        id: 2,
+        user: "Santa Claus",
+        title: "Christmas Move",
+        date: "25/12/18",
+        budget: "$9999",
+        addressTo: "North Pole",
+        addressFrom: "South Pole",
+        description: "Reindeers on holidays, need help :("
+    }
+]
+
+const sortByOptions = [
+    { key: 1, text: 'Best Match', value: 1},
+    { key: 2, text: 'Price', value: 2},
+    { key: 3, text: 'Most Recent', value: 3}
+]
 
 export default class CreateLoginForm extends Component {
     constructor() {
@@ -26,23 +62,28 @@ export default class CreateLoginForm extends Component {
             <div>
                 <Segment vertical><h1>Results</h1></Segment>
                 <Segment inverted tertiary>
-                    <Dropdown text='Price' floating labelled button>
-                        <Dropdown.Menu style={{paddingLeft: 10, paddingRight: 10, paddingBottom:10}}>
-                        <InputSlider
-                            value={this.state.budget}
-                            onChange={this.onBudgetChange}
-                            min={BUDGET.MIN}
-                            max={BUDGET.MAX}
-                            steps={10}
-                            icon={'dollar'}
-                        />
-                        </Dropdown.Menu>
-                    </Dropdown>
+                    <Menu secondary>
+                        <Dropdown text='Price' floating labelled button>
+                            <Dropdown.Menu style={{paddingLeft: 10, paddingRight: 10, paddingBottom:10}}>
+                            <InputSlider
+                                value={this.state.budget}
+                                onChange={this.onBudgetChange}
+                                min={BUDGET.MIN}
+                                max={BUDGET.MAX}
+                                steps={10}
+                                icon={'dollar'}
+                            />
+                            </Dropdown.Menu>
+                        </Dropdown>
+                        <Dropdown text='Date' floating labelled button/>
+                        <Menu.Menu position='right'>
+                            <h4>Sort by:</h4>
+                            <Dropdown selection defaultValue={sortByOptions[0].value} options={sortByOptions} compact floating labelled button />
+                        </Menu.Menu>
+                    </Menu>
                 </Segment>
                 <Segment secondary>
-                    <Segment vertical>Result 1</Segment>
-                    <Segment vertical>Result 2</Segment>
-                    <Segment vertical>Result 3</Segment>
+                    <PostList posts={results}/>
                 </Segment>
             </div>
         )
