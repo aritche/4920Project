6a9222c440a8fe247c41152ea417b9c89402295b
Ashton Zhuang <ashton.zhuang@student.unsw.edu.au>
2018-09-18 09:55:51 +1000

    added skeleton def functions, user has no bugs at the moment

--- back-end/database/User.py
+++ back-end/database/User.py
@@ -1,5 +1,6 @@
 from model import db
-
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
 # [START model]
 class User(db.Model):
     __tablename__ = 'user'
@@ -12,6 +13,8 @@ class User(db.Model):
     password = db.Column(db.String(255), nullable=False)
     creation_date = db.Column(db.String(255), nullable=False)
     user_type = db.Column(db.String(255), nullable=False)
+
+    address = relationship("Address", back_populates="user")
 
     def to_dict(self):
         return {
@@ -27,6 +30,42 @@ class User(db.Model):
         return "<User(email='%s')" % (self.email)
 
     def validate_email(self, key, email):
+        # need to add in more validation rules
         assert '@' in email
         return email
+
+    # def add_user(email, first_name, last_name, user_type):
+        # will create session
+        # validate email
+        # insert email
+        # validate names
+        # input names
+        # get/input unique id, address_id 
+        # input password
+        # input creation_date
+        # input user_type
+        # return successful or not
+
+    # def login_validation(email, password):
+        # check user table for email 
+        # check corresponding password
+        # return 0 for good, 1 for no user, 2 for wrong password
+
+    # def unique_id_generator(table):
+        # from table
+        # get the last id
+        # return the id + 1
+
+
+
 # [END model]
+
+
+
+
+
+
+
+
+
+
