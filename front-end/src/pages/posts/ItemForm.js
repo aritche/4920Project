aa220c22491349428ac0c82b8a65ab2e07f1c3f3
Victor <wuaiweijun@hotmail.com>
2018-09-19 17:58:55 +1000

    Fixed minor issue

--- front-end/src/pages/posts/ItemForm.js
+++ front-end/src/pages/posts/ItemForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Form, TextArea, Header, Modal, Button, Icon } from 'semantic-ui-react';
+import { Form, TextArea, Header, Modal, Button, Icon, Input } from 'semantic-ui-react';
 
 /**
  * Author: VW
@@ -20,38 +20,50 @@ export default class ItemForm extends Component {
 
     close = () => {
       this.setState({open: false});
-    }
+    };
 
     open = () => {
       this.setState({open: true});
-    }
+    };
 
     onNameChange = (value) => {
       this.setState({name: value});
-    }
+    };
 
     onWeightChange = (value) => {
         this.setState({weight: value});
-    }
+    };
 
     onVolumeChange = (value) => {
         this.setState({volume: value});
-    }
+    };
 
     onDescChange = (value) => {
       this.setState({desc: value});
-    }
+    };
 
     onAmountChange = (value) => {
       this.setState({amount: value});
-    }
+    };
 
     onSubmit = () => {
       //TODO: validation that fields are not empty
       // Make sure name is unique
-      const { name, weight, volume, desc, amount } = this.state;
-      this.props.addItem(name, weight, volume, desc, amount);
-      this.close();
+      if (!this.validation()) {
+          const { name, weight, volume, desc, amount } = this.state;
+          this.props.addItem(name, weight, volume, desc, amount);
+          this.close();
+      }
+      else {
+          //alert(this.validation);
+          //alert(this.state.name + " " + this.state.weight + " " + this.state.volume + " " + this.state.desc);
+          alert("Please fill all fields")
+      }
+    };
+
+    validation = () => {
+        const {name, weight, volume, amount, desc} = this.state;
+        return (name === "") || (weight === "") || (volume === "") || (amount === "") || (desc === "");
     };
 
     render() {
@@ -67,12 +79,21 @@ export default class ItemForm extends Component {
             <Header> Add Item </Header>
             <Modal.Content>
               <Form>
-                <Form.Group widths='equal'>
-                  <Form.Input fluid label='Name'  placeholder='Item Name' onChange={(e) => this.onNameChange(e.target.value)}/>
-                  <Form.Input fluid label='Estimated Weight'  placeholder='Item Weight' onChange={(e) => this.onWeightChange(e.target.value)}/>
-                  <Form.Input fluid label='Estimated Volume'  placeholder='Item Volume' onChange={(e) => this.onVolumeChange(e.target.value)}/>
-                  <Form.Input fluid label='Amount'  placeholder='Item Amount' onChange={(e) => this.onAmountChange(e.target.value)}/>
-                </Form.Group>
+                <div style={{display: 'flex'}}>
+                  <Input fluid placeholder='Item Name' onChange={(e) => this.onNameChange(e.target.value)}
+                              style={{width: 200}}/>
+                  <span style={{width: 20}}/>
+                  <Input fluid label={{ basic: true, content: 'kg' }}
+                              labelPosition='right'  placeholder='Item Weight'
+                              onChange={(e) => this.onWeightChange(e.target.value)} style={{width: 200}}/>
+                  <span style={{width: 20}}/>
+                  <Input fluid label={{ basic: true, content: 'm^3' }}
+                              labelPosition='right'  placeholder='Item Volume'
+                              onChange={(e) => this.onVolumeChange(e.target.value)} style={{width: 200}}/>
+                  <span style={{width: 20}}/>
+                  <Input fluid placeholder='Item Amount'
+                              onChange={(e) => this.onAmountChange(e.target.value)} style={{width: 200}}/>
+                </div>
                 <Header content={'Item Description'} />
                 <TextArea autoHeight onChange={(e) => this.onDescChange(e.target.value)}/>
               </Form>
