b8569f3b904c5f71fae7058dbc8c7c9401607b11
kangas113 <kangas113@gmail.com>
2018-09-19 23:04:45 +1000

    view individual post page frontend

--- front-end/src/Routes.js
+++ front-end/src/Routes.js
@@ -14,7 +14,7 @@ export default class Routes extends Component {
         return (
             <Switch>
               <Route exact path='/' component={HomePage}/>
-              <Route path='/posts/:id' component={PostDetailsPage}/>
+              <Route path='/posts/:postId' component={PostDetailsPage}/>
               <Route path='/posts' component={PostsPage}/>
               <Route path='/create-post' component={CreatePostPage}/>
               <Route path='/login' component={LoginPage}/>
--- front-end/src/pages/posts/PostDetailsPage.js
+++ front-end/src/pages/posts/PostDetailsPage.js
@@ -1,118 +1,134 @@
 import React, { Component } from 'react';
 import { Header, Container, Comment, Form, Button, Step, Icon } from 'semantic-ui-react';
 import { isLoggedIn, getLoggedInUser } from '../../Authentication';
+import { url } from '../../Api';
+import Loading from 'react-loading';
 
-// TODO: Get from backend
-const post = {
-    id: 1,
-    movee_id: 3,
-    user: "John Smith",
-    title: "Test post 1",
-    date: "10/09/18",
-    budget: "1000",
-    addressTo: "Suburb 1",
-    addressFrom: "Suburb 2",
-    description: `Lorem Ipsum is simply dummy text of 
-    the printing and typesetting 
-    industry. Lorem Ipsum has been
-    the industry's standard dummy text 
-    ever since the 1500s, when an unknown
-    printer took a galley of type and scrambled 
-    it to make a type specimen book
-    `
-}
 
 export default class PostDetailsPage extends Component {
-    render() {        
-        return (
-            <Container>
-                <Header as='h1'>
-                    { post.title }
-                    {
-                        isLoggedIn() && getLoggedInUser() === post.movee_id &&
-                        <Button style={{    verticalAlign: 'bottom',
-                            marginLeft: '30px'}} negative>Delete</Button>
-                    }
-                </Header>
-                <p className="heading-subtitle">By { post.user } <img className="heading-subtitle-icon" src='/images/default_profile_pic.jpg' alt="Default Profile"/></p>
+    constructor(props) {
+        super(props);
+        this.state = {
+            'isLoading': true,
+            'post': {
+                'id': props.match.params.postId,
+            }
+        }
+    }
 
-                <p> { post.description } </p>
-                <p> <b>Budget:</b> { '$' + post.budget } </p>
-                <p> <b>Date:</b> { post.date } </p>
+    componentDidMount() {
+        fetch(url + 'post/' + this.state.post.id).then(response => {
+            if (response.status === 200) {
+                response.json().then(obj => {
+                    this.setState({
+                        post: obj.move,
+                        isLoading: false
+                    })
+                    return;
+                });
+            } else {
+                this.setState({
+                    errorMessage: 'Sorry, there was a problem with your submission. Please try again.',
+                    isLoading: false
+                });
+            }
+        });
+    }
 
-                <Step.Group widths={2}>
-                    <Step active>
-                        <Icon name='truck' />
-                        <Step.Content>
-                            <Step.Title>From Address</Step.Title>
-                            <Step.Description>{ post.addressFrom }</Step.Description>
-                        </Step.Content>
-                    </Step>
-                    <Step active>
-                        <Icon name='truck' />
-                        <Step.Content>
-                            <Step.Title>To Address</Step.Title>
-                            <Step.Description>{ post.addressTo }</Step.Description>
-                        </Step.Content>
-                    </Step>
-                </Step.Group>
-
-                <Comment.Group>
-                    <Header as='h3' dividing>
-                    Comments
+    render() {
+        if (this.state.isLoading || this.state.errorMessage) {
+            return 'Loading...'
+        } else {
+            return (
+                <Container>
+                    <Header as='h1'>
+                        { this.state.post.title }
+                        {
+                            isLoggedIn() && getLoggedInUser() === this.state.post.movee.id &&
+                            <Button style={{    verticalAlign: 'bottom',
+                                marginLeft: '30px'}} negative>Delete</Button>
+                        }
                     </Header>
+                    <p className="heading-subtitle">By { this.state.post.movee.first_name + ' ' + this.state.post.movee.last_name } <img className="heading-subtitle-icon" src='/images/default_profile_pic.jpg' alt="Default Profile"/></p>
 
-                    <Comment>
-                    <Comment.Avatar src='/images/default_profile_pic.jpg' />
-                    <Comment.Content>
-                        <Comment.Author as='a'>Matt</Comment.Author>
-                        <Comment.Metadata>
-                        <div>Today at 5:42PM</div>
-                        </Comment.Metadata>
-                        <Comment.Text>Hey John, can we negotiate that pricing a bit?</Comment.Text>
-                        <Comment.Actions>
-                        <Comment.Action>Reply</Comment.Action>
-                        </Comment.Actions>
-                    </Comment.Content>
-                    </Comment>
+                    <p> { this.state.post.description } </p>
+                    <p> <b>Budget:</b> { '$' + this.state.post.budget } </p>
+                    <p> <b>Date:</b> { this.state.post.closing_datetime1 } </p>
 
-                    <Comment>
-                    <Comment.Avatar src='/images/default_profile_pic.jpg' />
-                    <Comment.Content>
-                        <Comment.Author as='a'>Elliot Fu</Comment.Author>
-                        <Comment.Metadata>
-                        <div>Yesterday at 12:30AM</div>
-                        </Comment.Metadata>
-                        <Comment.Text>
-                        <p>Could you give a little more detail about the flights of stairs we'll need to go up and down?</p>
-                        </Comment.Text>
-                        <Comment.Actions>
-                        <Comment.Action>Reply</Comment.Action>
-                        </Comment.Actions>
-                    </Comment.Content>
+                    <Step.Group widths={2}>
+                        <Step active>
+                            <Icon name='truck' />
+                            <Step.Content>
+                                <Step.Title>From Address</Step.Title>
+                                <Step.Description>{ this.state.post.address_from.city }</Step.Description>
+                            </Step.Content>
+                        </Step>
+                        <Step active>
+                            <Icon name='truck' />
+                            <Step.Content>
+                                <Step.Title>To Address</Step.Title>
+                                <Step.Description>{ this.state.post.address_to.city }</Step.Description>
+                            </Step.Content>
+                        </Step>
+                    </Step.Group>
+
                     <Comment.Group>
+                        <Header as='h3' dividing>
+                        Comments
+                        </Header>
+
                         <Comment>
                         <Comment.Avatar src='/images/default_profile_pic.jpg' />
                         <Comment.Content>
-                            <Comment.Author as='a'>John Smith</Comment.Author>
+                            <Comment.Author as='a'>Matt</Comment.Author>
                             <Comment.Metadata>
-                            <div>Just now</div>
+                            <div>Today at 5:42PM</div>
                             </Comment.Metadata>
-                            <Comment.Text>It's on the 1st floor, so 1 flight of stairs :)</Comment.Text>
+                            <Comment.Text>Hey John, can we negotiate that pricing a bit?</Comment.Text>
                             <Comment.Actions>
                             <Comment.Action>Reply</Comment.Action>
                             </Comment.Actions>
                         </Comment.Content>
                         </Comment>
-                    </Comment.Group>
-                    </Comment>
 
-                    <Form reply>
-                    <Form.TextArea />
-                    <Button content='Add Reply' labelPosition='left' icon='edit' primary />
-                    </Form>
-                </Comment.Group>
-            </Container>
-        )
+                        <Comment>
+                        <Comment.Avatar src='/images/default_profile_pic.jpg' />
+                        <Comment.Content>
+                            <Comment.Author as='a'>Elliot Fu</Comment.Author>
+                            <Comment.Metadata>
+                            <div>Yesterday at 12:30AM</div>
+                            </Comment.Metadata>
+                            <Comment.Text>
+                            <p>Could you give a little more detail about the flights of stairs we'll need to go up and down?</p>
+                            </Comment.Text>
+                            <Comment.Actions>
+                            <Comment.Action>Reply</Comment.Action>
+                            </Comment.Actions>
+                        </Comment.Content>
+                        <Comment.Group>
+                            <Comment>
+                            <Comment.Avatar src='/images/default_profile_pic.jpg' />
+                            <Comment.Content>
+                                <Comment.Author as='a'>John Smith</Comment.Author>
+                                <Comment.Metadata>
+                                <div>Just now</div>
+                                </Comment.Metadata>
+                                <Comment.Text>It's on the 1st floor, so 1 flight of stairs :)</Comment.Text>
+                                <Comment.Actions>
+                                <Comment.Action>Reply</Comment.Action>
+                                </Comment.Actions>
+                            </Comment.Content>
+                            </Comment>
+                        </Comment.Group>
+                        </Comment>
+
+                        <Form reply>
+                        <Form.TextArea />
+                        <Button content='Add Reply' labelPosition='left' icon='edit' primary />
+                        </Form>
+                    </Comment.Group>
+                </Container>
+            )
+        }
     }
 }
