0b4ca119381296a3ba4b6143d8d482a8c33ffe49
Ari <ari.tchetchenian@gmail.com>
2018-10-06 04:46:58 +1000

    basic home page

--- front-end/src/pages/home/CreateHome.js
+++ front-end/src/pages/home/CreateHome.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Signup from '../signup/CreateSignupForm';
+import { Segment, Container, Header, Grid } from 'semantic-ui-react';
 
 export default class CreateLoginForm extends Component {
     constructor() {
@@ -13,10 +14,23 @@ export default class CreateLoginForm extends Component {
 
     render() {
         return (
-            <div>
-                Home page (ari is working on this at the moment)
-                <Signup/>
-            </div>
+            <Container style={{height: '80vh', backgroundColor: '#193446'}}>
+                <Grid columns={2} centered>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <div className='centered-div'>
+                                <Header size='huge' style={{color:'white', fontSize: '4em'}}>uMove</Header>
+                                <Header as='h2' style={{color:'white'}}>Connecting movees and removalists</Header>
+                            </div>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <div className='centered-div'>
+                                <Signup/>
+                            </div>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </Container>
         )
     }
 }
