eda337feb347f00fd45f223500a2373c9444986f
Ari <ari.tchetchenian@gmail.com>
2018-10-07 01:41:05 +1000

    improved post table layout

--- front-end/src/pages/posts/Post.js
+++ front-end/src/pages/posts/Post.js
@@ -9,24 +9,30 @@ export default class Post extends Component {
 
         return (
             <Item key={ post.id } as={Link} to={"/posts/" + post.id}>
-                <Item.Image size='tiny' src='/images/default_profile_pic.jpg' />
+                <Item.Image circular size='tiny' src='/images/default_profile_pic.jpg' />
 
                 <Item.Content>
                     <Item.Header>{post.title}</Item.Header>
                     <Item.Meta>{post.movee.first_name + ' ' + post.movee.last_name}</Item.Meta>
                     <Item.Description>
                         <Grid>
-                            <Grid.Column width={4} >
+                            <Grid.Column width={3} >
                                 From { post.address_from.city }<br/>
                                 To { post.address_to.city }
                             </Grid.Column>
                             <Grid.Column width={2} >
-                                { post.budget }
+                                ${ post.budget }
                             </Grid.Column>
-                            <Grid.Column width={6} >
+                            <Grid.Column width={5} >
                                 <div className="post-description">
-                                    { post.description }
+                                    "{ post.description }"
                                 </div>
+                            </Grid.Column>
+                            <Grid.Column width={3} >
+                                [Stars go here]
+                            </Grid.Column>
+                            <Grid.Column width={3} >
+                                [Date goes here]
                             </Grid.Column>
                         </Grid>
                     </Item.Description>
