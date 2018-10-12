8cf90d23023a944186ba4e6774d886e1c8d889dc
Ashton Zhuang <ashton.zhuang@student.unsw.edu.au>
2018-10-12 18:19:29 +1100

    adjusting

--- back-end/api/reviews.py
+++ back-end/api/reviews.py
@@ -3,11 +3,10 @@ from datetime import datetime
 from database.model import db
 from database.User import User
 from database.MoveDetails import MoveDetails
-from database.Reviews import Reviews
+from database.Review import Review
 from sqlalchemy import and_, not_
 
-
-def insert_new_review(json):
+def insert_new_movee_review(json):
     if (
         not json
         or (not 'poster' in json)
@@ -19,12 +18,11 @@ def insert_new_review(json):
     ):
         abort(400, 'Not all fields were received.')
 
-    # Validate Poster and Reviewed User are both not the same type of user
+    # Validate Poster and Reviewed User are both in database
+    if json['poster'] == json['reviewedUser']:
+        abort(400, 'Invalid review, poster and reviewed user cannot be the same')
 
-    # query_result = db.session.query(User).filter(and_(User.email == json['email'], not User.deleted)).first()
-    # if query_result:
-    #     abort(400, 'A user with this email address already exists.')
-    # query_result = db.session.query(User).filter(and_(User.user_type == json['poster'], not (User.id == json[])))
+    # Validate Poster and Reviewed User are both not the same type of user
 
     # Validate poster and reviewed user are real
     query_poster = db.session.query(User).filter(User.id == json['poster']).first()
@@ -36,16 +34,12 @@ def insert_new_review(json):
     if not query_reviewedUser:
     	abort(400, 'Invalid review, reviewed user do not exist')
 
-    # Validate Poster and Reviewed User are both in database
-    if json['poster'] == json['reviewedUser']:
-    	abort(400, 'Invalid review, poster and reviewed user cannot be the same')
-
     # Validate move exists
     query_result = db.session.query(MoveDetails).filter(MoveDetails.id == json['move']).first()
     if not query_result:
     	abort(400, 'Move does not exist')
 
-    review = review(
+    review = Review(
         poster = json['poster'],
         reviewedUser = json['reviewedUser'],
         move = json['move'],
@@ -71,7 +65,7 @@ def remove_review(json):
     else:
         abort(400, 'Not all required fields were received.')
 
-    review_to_delete = db.session.query(Reviews).filter(and_(Reviews.id == id_to_delete, not_(Reviews.deleted))).first()
+    review_to_delete = db.session.query(Review).filter(and_(Review.id == id_to_delete, not_(Review.deleted))).first()
 
     if not review_to_delete:
         abort(400, 'Review not found.')
@@ -84,3 +78,25 @@ def remove_review(json):
         resp.status_code = 200
 
         return resp
+
+def get_average_rating(id):
+    # query_result = db.session.query(User).filter(User.id == json['id']).first()
+    user_type = db.session.select([User.user_type]).where(User.id == id)
+    print(user_type)
+    print(type(user_type))
+    if user_type == "Movee":
+        list_of_ratings = db.session.query([Review.rating]).filter(Review.reviewedUser == id).all()
+        sum_of_ratings = 0
+        for item in list_of_ratings:
+            sum_of_ratings += item
+        average_rating = sum_of_ratings/len(list_of_ratings)
+    return average_rating
+
+
+
+
+
+
+
+
+
