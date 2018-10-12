b788527c55c3fab6979530d07d3afc483a5510fe
Ari <ari.tchetchenian@gmail.com>
2018-10-13 03:20:33 +1100

    aesthics

--- front-end/src/pages/home/HowItWorks.js
+++ front-end/src/pages/home/HowItWorks.js
@@ -41,35 +41,37 @@ export default class HowItWorks extends Component {
         return (
             <Segment attached secondary style={{borderRadius: '4px'}}>
                 <Header size='huge' block style={{backgroundColor: '#193446', color:'white'}}>How it works</Header>
+
                 <Grid textAlign='center' columns={4}>
                     <Grid.Row verticalAlign='middle'>
-                    <ProcessStep/>
+                        <ProcessStep/>
                     </Grid.Row>
                     <Grid.Row verticalAlign='middle'>
                         <Grid.Column>
-                          <Image src='/images/post.jpg' size='small' />
+                          <Image src='/images/post.jpg' size='medium' />
                         </Grid.Column>
                         <Grid.Column>
-                          Movees who need things to be moved post jobs with a budget!
-                        </Grid.Column>
-                    </Grid.Row>
-                    <Grid.Row verticalAlign='middle'>
-                        <Grid.Column>
-                          <Image src='/images/handshake.jpg' size='small' />
-                        </Grid.Column>
-                        <Grid.Column>
-                          Removalists offer their services at a price
+                          <strong>Movees who need things to be moved post jobs with a budget!</strong>
                         </Grid.Column>
                     </Grid.Row>
                     <Grid.Row verticalAlign='middle'>
                         <Grid.Column>
-                          <Image src='/images/moving box.jpg' size='small' />
+                          <Image src='/images/handshake.jpg' size='medium' />
                         </Grid.Column>
                         <Grid.Column>
-                          Movees accept an offer and removalists get the job done!
+                          <strong>Removalists offer their services at a price</strong>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row verticalAlign='middle'>
+                        <Grid.Column>
+                          <Image src='/images/moving box.jpg' size='medium' />
+                        </Grid.Column>
+                        <Grid.Column>
+                          <strong>Movees accept an offer and removalists get the job done!</strong>
                         </Grid.Column>
                     </Grid.Row>
                 </Grid>
+
             </Segment>
         )
     }
