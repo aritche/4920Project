d6e9f691d6a402bbd4d2763bdc68dc7ca3a8ebbe
Ari <ari.tchetchenian@gmail.com>
2018-10-14 02:14:07 +1100

    Recent Moves post list fixed

--- front-end/src/pages/posts/PostListSmall.js
+++ front-end/src/pages/posts/PostListSmall.js
@@ -5,22 +5,20 @@ import { Item, Message, Grid } from 'semantic-ui-react';
 export default class PostListSmall extends Component {
     render() {
         return (
-                this.props.posts && this.props.posts.length > 0 ?
-                    <div className="list-container">
-                    
-                        <Grid divided>
-                            <Grid.Column>
-                                {
-                                    this.props.posts.map(post => <PostSmall key={post.id} post={post} />)
-                                }
-                            </Grid.Column>
-                        </Grid>
-                    </div>
-                :
-                <Message>
-                    <Message.Header>No posts to view</Message.Header>
-                    <p>There are no posts currently with your search criteria, why not create one?</p>
-                </Message>
+            this.props.posts && this.props.posts.length > 0 ?
+                <div className="list-container">
+                
+                    <Grid link columns={this.props.posts.length}>
+                        {
+                            this.props.posts.map(post => <Grid.Column><PostSmall key={post.id} post={post} /></Grid.Column>)
+                        }
+                    </Grid>
+                </div>
+            :
+            <Message>
+                <Message.Header>No posts to view</Message.Header>
+                <p>There are no posts currently with your search criteria, why not create one?</p>
+            </Message>
         )
     }
 }
--- front-end/src/pages/posts/PostSmall.js
+++ front-end/src/pages/posts/PostSmall.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Item, Grid, Label, Icon, Rating} from 'semantic-ui-react';
+import { Item, Grid, Label, Icon, Rating, Container, Header} from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import './Post.css';
 
@@ -8,33 +8,33 @@ export default class PostSmall extends Component {
         const { post } = this.props;
 
         return (
-            <Item key={ post.id } as={Link} to={"/posts/" + post.id}>
-                <Item.Content>
-                    <Grid columns={1}>
+            <Container key={ post.id } as={Link} to={"/posts/" + post.id} style={{borderRadius: '20px', backgroundColor: '#fbfbfb', border: '2px solid black', color:'black', padding: '20px', width: '200px'}}>
+                <Grid columns={1} align='middle'>
+                    <Container>
+                        <Header as='h3'>
                         <Grid.Row>
-                            <Item.Header>
-                                {post.address_from.city}
-                                <Icon name='angle right'/>
-                                {post.address_to.city}
-                            </Item.Header>
+                            {post.address_from.city}
+                            <Icon name='angle right'/>
+                            {post.address_to.city}
+                        </Grid.Row>
+                        </Header>
+                    </Container>
+
+                    <Container>
+                        <Grid.Row>
+                            {post.closing_datetime1}
                         </Grid.Row>
 
-                        <Item.Description>
-                            <Grid.Row>
-                                ${ post.budget }
-                            </Grid.Row>
+                        <Grid.Row>
+                            ${ post.budget }
+                        </Grid.Row>
 
-                            <Grid.Row>
-                                <Rating maxRating={5} defaultRating={5} disabled icon='star'/>
-                            </Grid.Row>
-
-                            <Grid.Row>
-                                {post.closing_datetime1}
-                            </Grid.Row>
-                        </Item.Description>
-                    </Grid>
-                </Item.Content>
-            </Item>
+                        <Grid.Row>
+                            <Rating maxRating={5} defaultRating={5} disabled icon='star'/>
+                        </Grid.Row>
+                    </Container>
+                </Grid>
+            </Container>
         )
     }
 }
