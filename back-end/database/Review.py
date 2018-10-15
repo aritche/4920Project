9eb5123802031a9bcc886dee55adc241a4f61810
Ashton Zhuang <ashton.zhuang@student.unsw.edu.au>
2018-10-15 20:48:10 +1100

    fixed reviews based of Kangs comments

--- back-end/database/Review.py
+++ back-end/database/Review.py
@@ -2,16 +2,20 @@ from database.model import db
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 
-class Comment(db.Model):
-    __tablename__ = 'comment'
+class Review(db.Model):
+    __tablename__ = 'review'
 
     id = db.Column(db.Integer, primary_key=True)
     poster = db.Column(db.Integer, ForeignKey('user.id'), nullable=False)
     reviewed_user = db.Column(db.Integer, ForeignKey('user.id'), nullable=False)
     move = db.Column(db.Integer, ForeignKey('move.id')) # not sure if needed
     creation_datetime = db.Column(db.DateTime, nullable=False)
+    rating_general = db.Column(db.Integer, nullable=True)
+    rating_reliability = db.Column(db.Integer, nullable=True)
+    rating_service = db.Column(db.Integer, nullable=True)
+    rating_speed = db.Column(db.Integer, nullable=True)
     review = db.Column(db.String(500), nullable=False)
-    rating = db.Column(db.Integer, nullable=False)
+    deleted = db.Column(db.Boolean, nullable=False)
 
     def to_dict(self):
         return {
@@ -19,6 +23,10 @@ class Comment(db.Model):
             'poster': self.poster,
             'reviewed_user': self.reviewed_user,
             'creation_datetime': self.creation_datetime,
+            'rating_speed': self.rating_speed,
+            'rating_reliability': self.rating_reliability,
+            'rating_service': self.rating_service,
+            'rating_general': self.rating_general,
             'review': self.review,
-            'rating': self.rating,
+            'deleted': self.deleted
         }
