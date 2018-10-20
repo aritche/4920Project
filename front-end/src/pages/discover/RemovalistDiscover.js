15a59c333651883e5b2d8bfae187069dbab80abc
Ari <ari.tchetchenian@gmail.com>
2018-10-20 20:42:03 +1100

    skeleton for removalist discovery

--- front-end/src/pages/discover/RemovalistDiscover.js
+++ front-end/src/pages/discover/RemovalistDiscover.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-import { Container } from 'semantic-ui-react';
+import { Container, Segment, Header } from 'semantic-ui-react';
 
 export default class RemovalistDiscover extends Component {
     render() {
         return (
-            <Container>
-                Removalist
-            </Container>
+            <Segment attached style={{backgroundColor: "white", borderRadius: '4px'}}>
+                <Header attached='top' size={'huge'} block style={{backgroundColor: '#193446', color: 'white'}}>
+                Recommended Moves
+                </Header>
+                <Segment attached secondary>
+                     
+                </Segment>
+            </Segment>
         )
     }
 }
